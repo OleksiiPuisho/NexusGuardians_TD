9524e57241a7b560baa1ed25a22ea24293126fad
OleksiiPuisho <POO@d.c>
2023-09-12 20:09:28 +0200

    refactoring CameraController

--- Assets/_Scripts/CameraMovement/CameraController.cs
+++ Assets/_Scripts/CameraMovement/CameraController.cs
@@ -6,13 +6,18 @@ using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] private Transform _cameraTransform;
     [SerializeField] private float _cameraSpeedOffset;
     [SerializeField] private float _cameraInterpolate;
     [SerializeField] private float _distanceCamera;
 
     [SerializeField] private float _speedZoom;
+    [SerializeField] private Vector2 _clampZoom;
 
     private Vector3 _targetPosition;
+    private Vector3 _targetRotation;
+    private float _cameraRotation;
+    [SerializeField] private Vector2 _clampCameraX;
 
     private Touch _touchOne;
     private Touch _touchTwo;
@@ -20,14 +25,23 @@ public class CameraController : MonoBehaviour
     void Awake()
     {
         _targetPosition = transform.position;
+        _targetRotation = transform.rotation.eulerAngles;
+        _cameraRotation = _cameraTransform.localEulerAngles.x;
     }
 
     void LateUpdate()
     {
-        
         if (transform.position != _targetPosition)
         {
-            transform.position = Vector3.Lerp(_targetPosition, transform.position, _cameraInterpolate);
+            transform.position = Vector3.LerpUnclamped(transform.position, _targetPosition, _cameraInterpolate);
+        }
+        if (transform.rotation.eulerAngles != _targetRotation)
+        {
+            transform.rotation = Quaternion.LerpUnclamped(transform.rotation, Quaternion.Euler(_targetRotation), _cameraInterpolate);
+        }
+        if(_cameraTransform.localEulerAngles.x != _cameraRotation)
+        {
+            _cameraTransform.localEulerAngles = new(Mathf.Lerp(_cameraTransform.localEulerAngles.x, _cameraRotation, _cameraInterpolate), 0, 0);
         }
     }
     private void Update()
@@ -35,6 +49,7 @@ public class CameraController : MonoBehaviour
         ZoomHandler();
         CorrectedDistance();
         MovementHandler();
+        RotationHandler();
     }
 
     private void MovementHandler()
@@ -49,36 +64,58 @@ public class CameraController : MonoBehaviour
 #if UNITY_EDITOR
         if (Input.GetKey(KeyCode.W))
         {
-            _targetPosition = new Vector3(_targetPosition.x, _targetPosition.y, _targetPosition.z + _cameraSpeedOffset);
+            _targetPosition = GetDirectionMovement(transform.forward);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            _targetPosition = new Vector3(_targetPosition.x, _targetPosition.y, _targetPosition.z - _cameraSpeedOffset);
+            _targetPosition = GetDirectionMovement(-transform.forward);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            _targetPosition = new Vector3(_targetPosition.x - _cameraSpeedOffset, _targetPosition.y, _targetPosition.z);
+            _targetPosition = GetDirectionMovement(-transform.right);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            _targetPosition = new Vector3(_targetPosition.x + _cameraSpeedOffset, _targetPosition.y, _targetPosition.z);
+            _targetPosition = GetDirectionMovement(transform.right);
         }
 #endif
         #endregion
     }
 
+    private Vector3 GetDirectionMovement(Vector3 direction)
+    {
+        return Vector3.MoveTowards(_targetPosition, _targetPosition + direction, _cameraSpeedOffset);
+    }
+
     private void RotationHandler()
     {
         #region TouchControl
-        #if (PLATFORM_ANDROID)
+#if (PLATFORM_ANDROID)
 
-        #endif
+#endif
         #endregion
 
         #region EditorControl
-        #if UNITY_EDITOR
-
-        #endif
+#if UNITY_EDITOR
+        if (Input.GetKey(KeyCode.E))
+        {
+            _targetRotation = new(_targetRotation.x, _targetRotation.y + _cameraSpeedOffset, _targetRotation.z);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            _targetRotation = new(_targetRotation.x, _targetRotation.y - _cameraSpeedOffset, _targetRotation.z);
+        }
+        if(Input.GetKey(KeyCode.R))
+        {
+            _cameraRotation -= _cameraSpeedOffset;
+            _cameraRotation = Mathf.Clamp(_cameraRotation, _clampCameraX.x, _clampCameraX.y);
+        }
+        if (Input.GetKey(KeyCode.T))
+        {
+            _cameraRotation += _cameraSpeedOffset;
+            _cameraRotation = Mathf.Clamp(_cameraRotation, _clampCameraX.x, _clampCameraX.y);
+        }
+#endif
         #endregion
     }
 
@@ -108,7 +145,7 @@ public class CameraController : MonoBehaviour
         if (Input.mouseScrollDelta.y != 0)
         {
             _distanceCamera -= Input.mouseScrollDelta.y * _speedZoom;
-            _distanceCamera = Mathf.Clamp(_distanceCamera, 3f, 30f);
+            _distanceCamera = Mathf.Clamp(_distanceCamera, _clampZoom.x, _clampZoom.y);
         }
 #endif
         #endregion
